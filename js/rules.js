7171cdef0d1b5a83f8607bcfda6239ca9324c526
Sagnik S Roy <sagnik.sinharoy@gmail.com>
2018-12-15 20:58:02 -0500

    Final Changes

--- js/rules.js
+++ js/rules.js
@@ -13,6 +13,11 @@ function scrollFunction() {
 
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
+    $('html,body').animate({ scrollTop: 0 }, 'slow');
+    return false; 
 }
+
+$(function() {
+    // Implement flip
+    $('.piece-card').flip();
+});
